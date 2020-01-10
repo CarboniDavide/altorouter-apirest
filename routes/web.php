82065df17cea5b63e6288dbdcf28a39a1b9a9e74
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-10 02:10:50 +0100

    use uppercase foreach classes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@
  * Time: 22:12
  */
 
-$router->map( 'GET', '/products', 'productControllerWeb#index', 'read_products_web');
-$router->map( 'GET', '/categories', 'categoryControllerWeb#index', 'read_categories_web');
-$router->map( 'GET', '/', 'homeControllerWeb#index', 'home_page');
+$router->map( 'GET', '/products', 'ProductControllerWeb#index', 'read_products_web');
+$router->map( 'GET', '/categories', 'CategoryControllerWeb#index', 'read_categories_web');
+$router->map( 'GET', '/', 'HomeControllerWeb#index', 'home_page');
