2546d8c7f2534b4b79d7385601e85bb6e73ebf60
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-11 00:04:55 +0100

    use redBean for each action

--- controllers/api/productController.php
+++ controllers/api/productController.php
@@ -47,82 +47,18 @@ class ProductController
 
     private function existe($id)
     {
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $p = new Product($db);
-
-        // set ID property of product to be edited
-        $p->id = $id;
-
-        // read the details of product to be edited
-        $p->readOne();
-
-        if ($p->rows == 0){
-            return false;
-        }
-        return true;
+        return R::findOne('products','id = ?', [$id]) ? true : false;
     }
 
     public function read_one($param)
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: access");
-        header("Access-Control-Allow-Methods: GET");
-        header("Access-Control-Allow-Credentials: true");
-        header('Content-Type: application/json');
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
-
-        // set ID property of product to be edited
-        $product->id = $param['id'];
-
-        // read the details of product to be edited
-        $product->readOne();
-
-        if ($product->rows == 0){
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(
-                array("error"=>"not_found", "error_description"=>"There is no items to read"),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
-
-        // create array
-        $product_arr = array(
-            "id" => $product->id,
-            "name" => $product->name,
-            "description" => $product->description,
-            "price" => $product->price,
-            "modified" => $product->modified,
-            "created" => $product->created,
-            "category_id" => $product->category_id
-        );
-
-        // make it json format
-        echo json_encode($product_arr, JSON_PRETTY_PRINT);
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode(R::getRow('SELECT * FROM products WHERE id = ?', [$param['id']]), JSON_PRETTY_PRINT);
     }
 
     public function create(){
         // required headers
-        header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
-        header("Access-Control-Allow-Methods: POST");
-        header("Access-Control-Max-Age: 3600");
-        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-        $database = new Database();
-        $db = $database->getConnection();
-
-        $product = new Product($db);
 
         // get posted data
         $data = json_decode(file_get_contents("php://input"));
@@ -144,6 +80,8 @@ class ProductController
             return;
         }
 
+        // create a new bean
+        $product= R::dispense( 'products' );
         // set product property values
         $product->name = $data->name;
         $product->price = $data->price;
@@ -151,14 +89,11 @@ class ProductController
         $product->category_id = $data->category_id;
         $product->created = $data->created;
         $product->modified = $data->modified;
-
-        // create the product
-        if($product->create()){
+        // store and check
+        if(R::store( $product )){
             header("HTTP/1.1 201 OK Created");
-            echo json_encode(array("message"=>"Product was created."),JSON_PRETTY_PRINT);
+            echo json_encode(array("message"=>"Product was created."), JSON_PRETTY_PRINT);
         }
-
-        // if unable to create the product, tell the user
         else{
             header("HTTP/1.1 400 Error");
             echo json_encode(array("message"=>"Unable to create product."),JSON_PRETTY_PRINT);
@@ -167,67 +102,33 @@ class ProductController
 
     public function delete($params){
         // required headers
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: DELETE");
-        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        header("Content-Type: application/json; charset=UTF-8");
 
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
-
-        // get product id
-        //$data = json_decode(file_get_contents("php://input"));
-
-        // set product id to be deleted
-        //$product->id = $data->id;
-        $product->id = $params['id'];
-
-        if (!self::existe($product->id))
-        {
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(array("error"=>"not_found","error_description"=>"There is no items to delete"),JSON_PRETTY_PRINT);
-            return;
-        }
-
-        // delete the product
-        if($product->delete()){
+        // delete product and check
+        // hunt return the number of delected rows elsewhere 0
+        if(R::hunt('products','id = ?', [ $params['id'] ])){  
             header("HTTP/1.1 202 OK Deleted");
-            echo json_encode(array("message"=>"Product was deleted."),JSON_PRETTY_PRINT);
+            echo json_encode(array("message"=>"Product was deleted."), JSON_PRETTY_PRINT);
         }
-
-        // if unable to delete the product
         else{
             header("HTTP/1.1 400 Error");
-            echo json_encode(array("message"=>"Unable to delete object."),JSON_PRETTY_PRINT);
+            echo json_encode(array("message"=>"Unable to delete object."), JSON_PRETTY_PRINT);
         }
     }
 
     public function update($params)
     {
         // required headers
-        header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
-        header("Access-Control-Allow-Methods: PUT");
-        header("Access-Control-Max-Age: 3600");
-        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
 
         // get id of product to be edited
         $data = json_decode(file_get_contents("php://input"));
 
-        // set ID property of product to be edited
-        $product->id = $params['id'];
+        // get product to update
+        $product = R::findOne('products', 'id = ?', [ $params['id'] ]);
 
-        if (!self::existe($product->id))
+        // check if exists -> product is null
+        if (!$product)
         {
             header("HTTP/1.1 404 Not Found");
             echo json_encode(array("error"=>"not_found","error_description"=>"There is no items to update"),
@@ -236,253 +137,76 @@ class ProductController
             return;
         }
 
-        //verify Json file
-        if (
-            !isset($data->name) ||
-            !isset($data->price) ||
-            !isset($data->description) ||
-            !isset($data->category_id) ||
-            !isset($data->created) ||
-            !isset($data->modified)
-        ){
-            header("HTTP/1.1 400 Bad request");
-            echo json_encode(
-                array("error"=>"bad_request", "error_description"=>"No enough information in Json file "),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
+        // update product with the new values elsewhere use older values
+        $product->name = !isset($data->name) ? $product->name : $data->name;
+        $product->price = !isset($data->price) ? $product->price :  $data->price;
+        $product->description = !isset($data->description) ? $product->description :  $data->description;
+        $product->modified = !isset($data->modified) ? $product->modified :  $data->modified;
+        $product->created = !isset($data->created) ? $product->created :  $data->created;
+        $product->category_id = !isset($data->category_id) ? $product->category_id :  $data->category_id;
 
-        // set product property values
-        $product->name = $data->name;
-        $product->price = $data->price;
-        $product->description = $data->description;
-        $product->modified = $data->modified;
-        $product->created = $data->created;
-        $product->category_id = $data->category_id;
-
-        // update the product
-        if($product->update()){
+        // update and check
+        if(R::store( $product )){
             header("HTTP/1.1 201 OK Updated");
-            echo json_encode(array("message"=>"Product was updated."),JSON_PRETTY_PRINT);
+            echo json_encode(array("message"=>"Product was updated."), JSON_PRETTY_PRINT);
         }
-
-        // if unable to update the product, tell the user
         else{
             header("HTTP/1.1 400 Error");
-            echo json_encode(array("message"=>"Unable to update product."),JSON_PRETTY_PRINT);
+            echo json_encode(array("message"=>"Unable to update product."), JSON_PRETTY_PRINT);
         }
     }
 
     public function search()
     {
         // required headers
-        header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
 
-        if (empty($_GET)){
+        extract($_GET);
+
+        // check for empty query string
+        if ( !isset($name) && !isset($description) && !isset($price) ){
             header("HTTP/1.1 400 Bad Request");
             echo '{"error":"invalid_request", "error_description":"There is no property"}';
             return;
         }
 
-        #create Keywords to search
-        $keywords = array();
+        // load default and sinitize
+        $_name = isset($name) ? htmlspecialchars(strip_tags($name)) : '';
+        $_description = isset($description) ? htmlspecialchars(strip_tags($description)) : '';
+        $_price = isset($price) ? htmlspecialchars(strip_tags($price)) : '';
 
-        extract($_GET);
-
-        $keywords['name'] = isset($name) ? $name : '';
-        $keywords['description'] = isset($description) ? $description : '';
-        $keywords['price'] = isset($price) ? $price : '';
-
-        if (($keywords['name'] == '') && ($keywords['description'] == '') && ($keywords['price'] == '')){
-            header("HTTP/1.1 400 Bad Request");
-            echo json_encode(
-                array("error" => "invalid_request", "error_description" => "There is no property"),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
-
-        // instantiate database and product object
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // initialize object
-        $product = new Product($db);
-
-        // query products
-        $stmt = $product->search($keywords);
-        $num = $stmt->rowCount();
-
-        // check if more than 0 record found
-        if($num>0){
-
-            // products array
-            $products_arr=array();
-
-            // retrieve our table contents
-            // fetch() is faster than fetchAll()
-            // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                // extract row
-                // this will make $row['name'] to
-                // just $name only
-                extract($row);
-
-                $product_item=array(
-                    "id" => $id,
-                    "name" => $name,
-                    "description" => html_entity_decode($description),
-                    "price" => $price,
-                    "modified" => $modified,
-                    "created" => $created,
-                    "category_id" => $category_id
-                );
-
-                array_push($products_arr, $product_item);
-            }
-
-            echo json_encode($products_arr, JSON_PRETTY_PRINT);
-        }
-
-        else{
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(
-                array("error" => "not_found", "error_description" => "There is no items"),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
-    }
-
-    private function readLimit($params)
-    {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: access");
-        header("Access-Control-Allow-Methods: GET");
-        header("Access-Control-Allow-Credentials: true");
-        header('Content-Type: application/json');
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
-
-        // set ID property of product to be edited
-
-        // read the details of product to be edited
-        $params == "first" ? $product->readFirst() : $product->readLast();
-
-        if ($product->name == null){
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(
-                array("error"=>"not_found", "error_description"=>"There is no items"),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
-
-        // create array
-        $product_arr = array(
-            "id" => $product->id,
-            "name" => $product->name,
-            "description" => $product->description,
-            "price" => $product->price,
-            "modified" => $product->modified,
-            "created" => $product->created,
-            "category_id" => $product->category_id
+        echo json_encode(
+            R::getAll('SELECT * FROM products WHERE name LIKE ? AND description LIKE ? AND price LIKE ? ORDER BY name ASC', ["%$_name%", "%$_description%", "%$_price%"] ), 
+            JSON_PRETTY_PRINT
         );
-
-        // make it json format
-        echo json_encode($product_arr,JSON_PRETTY_PRINT);
-
     }
 
     public function readFirst()
     {
-        self::readLimit("first");
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode(R::getRow('SELECT * FROM products ORDER BY id ASC LIMIT 0,1'), JSON_PRETTY_PRINT);
     }
 
     public function readLast()
     {
-        self::readLimit("last");
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode(R::getRow('SELECT * FROM products ORDER BY id DESC LIMIT 0,1'), JSON_PRETTY_PRINT);
     }
 
     public function count()
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: access");
-        header("Access-Control-Allow-Methods: GET");
-        header("Access-Control-Allow-Credentials: true");
-        header('Content-Type: application/json');
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
-
-        // set ID property of product to be edited
-
-        // read the details of product to be edited
-        $count = array("" => $product->count());
-        echo json_encode($count);
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode(R::getRow('SELECT COUNT(*) as total_rows FROM products'), JSON_PRETTY_PRINT);
     }
 
 
     public function read_product_as_category($params)
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: access");
-        header("Access-Control-Allow-Methods: GET");
-        header("Access-Control-Allow-Credentials: true");
-        header('Content-Type: application/json');
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-
-        // prepare product object
-        $product = new Product($db);
-
-        // assign the id to search
-        $product->id = $params['id'];
-
-        // query products
-        $stmt = $product->read_product_as_category();
-        $num = $stmt->rowCount();
-
-        // check if more than 0 record found
-        if ($num > 0) {
-
-            // products array
-            $categories_arr["categories"] = array();
-
-            // retrieve our table contents
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                // extract row
-                // this will make $row['name'] to
-                // just $name only
-                extract($row);
-
-                $category_item = $name;
-
-                array_push($categories_arr["categories"], $category_item);
-            }
-
-            echo json_encode($categories_arr);
-        } else {
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(
-                array("error" => "not_found", "error_description" => "There is no items"),
-                JSON_PRETTY_PRINT
-            );
-            return;
-        }
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode(
+            R::getAll('SELECT c.name, c.id FROM products p LEFT JOIN categories c ON p.category_id = c.id WHERE p.id = :id ORDER BY p.id ASC', [':id' => $params['id']] ), 
+            JSON_PRETTY_PRINT
+        );
     }
 
     private function read_pagination($from_record_num, $records_per_page, $home_url, $page){
