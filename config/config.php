47e3a00616ad9337d7d49441e839f1ce4533ba10
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-11 04:15:55 +0100

    reset config file

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 
 
 define('M_DB_NAME', 'api_db');
-define('M_DB_USER', 'test');
-define('M_DB_PASS', 'test');
-define('M_DB_HOST', 'localhost');
+define('M_DB_USER', '');
+define('M_DB_PASS', '');
+define('M_DB_HOST', '');
 define('M_DB_EXTRA', '');
 define('M_DB_DRIVER', 'mysql');
