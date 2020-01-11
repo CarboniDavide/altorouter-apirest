9daf8d39b7dc6fd061d70253f68f336e8af19d8c
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-11 03:19:03 +0100

    use bootstrap for table and header

--- views/categories.php
+++ views/categories.php
@@ -1,21 +1,31 @@
-<div class="content">
-    <h2>Categories</h2>
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Created</th>
-            <th>Modified</th>
-        </tr>
-        <?php foreach ($categories as $category) { ?>
-        <tr>
-            <td><?= $category['id'] ?></td>
-            <td><?= $category['name'] ?></td>
-            <td><?= $category['description'] ?></td>
-            <td><?= $category['created'] ?></td>
-            <td><?= $category['modified'] ?></td>
-        </tr>
-        <?php } ?>
+
+<div class="container">
+    
+    <div class="d-flex align-items-center justify-content-between mt-5 mb-5">
+        <h1>Categories</h1>
+        <a href="/"><button class="btn btn-primary">back</button></a>
+    </div>
+    
+    <table class="table table-striped">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Created</th>
+                <th scope="col">Modified</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($categories as $category) { ?>
+                <tr>
+                    <td><?= htmlspecialchars(strip_tags($category['id'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($category['name'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($category['description'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($category['created'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($category['modified'])) ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
     </table>
 </div>
--- views/products.php
+++ views/products.php
@@ -1,25 +1,34 @@
-<div class="content">
-    <h2>Products</h2>
-    <table class="dudeTable">
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Created</th>
-            <th>Modified</th>
-            <th>Category</th>
-        </tr>
-        <?php foreach ($products as $product) { ?>
-        <tr>
-            <td><?= $product['id'] ?></td>
-            <td><?= $product['name'] ?></td>
-            <td><?= $product['description'] ?></td>
-            <td><?= $product['price'] ?></td>
-            <td><?= $product['created'] ?></td>
-            <td><?= $product['modified'] ?></td>
-            <td><?= $product['category_id'] ?></td>
-        </tr>
-        <?php } ?>
+<div class="container">
+    
+    <div class="d-flex align-items-center justify-content-between mt-5 mb-5">
+        <h1>Products</h1>
+        <a href="/"><button class="btn btn-primary">back</button></a>
+    </div>
+
+    <table class="table table-striped">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Created</th>
+                <th scope="col">Modified</th>
+                <th scope="col">Category</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($products as $product) { ?>
+                <tr>
+                    <td><?= htmlspecialchars(strip_tags($product['id'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['name'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['description'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['price'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['created'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['modified'])) ?></td>
+                    <td><?= htmlspecialchars(strip_tags($product['category_id'])) ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
     </table>
 </div>
