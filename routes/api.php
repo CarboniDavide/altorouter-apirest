ebc4aae31eca2b381efabb6f4cfdd9546ef4aaa9
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-14 17:55:20 +0100

    rename categoriy's routes

--- routes/api.php
+++ routes/api.php
@@ -11,27 +11,27 @@ $prefix = "/api/v1";
 $router->map( 'GET', $prefix.'/', 'ApiController#index', 'api_index');
 
 # Routes API Product
-$router->map( 'GET', $prefix.'/products', 'ProductController#index', 'products_index');
-$router->map( 'GET', $prefix.'/products/[i:id]', 'ProductController#show', 'products_show');
+$router->map( 'GET', $prefix.'/products', 'ProductController#index', 'product_index');
+$router->map( 'GET', $prefix.'/products/[i:id]', 'ProductController#show', 'product_show');
 $router->map( 'GET', $prefix.'/products/first', 'ProductController#first', 'product_first');
 $router->map( 'GET', $prefix.'/products/last', 'ProductController#last', 'product_last');
-$router->map( 'GET', $prefix.'/products/count', 'ProductController#count', 'products_count');
-$router->map( 'GET', $prefix.'/products/search', 'ProductController#search', 'products_search');
+$router->map( 'GET', $prefix.'/products/count', 'ProductController#count', 'product_count');
+$router->map( 'GET', $prefix.'/products/search', 'ProductController#search', 'product_search');
 $router->map( 'GET', $prefix.'/products/[i:id]/categories', 'ProductController#product_category', 'products_as_category');
 
-$router->map( 'PUT', $prefix.'/products/[i:id]', 'ProductController#update', 'update_product');
-$router->map( 'DELETE', $prefix.'/products/[i:id]', 'ProductController#delete', 'delete_product');
-$router->map( 'POST', $prefix.'/products', 'ProductController#create', 'create_product');
+$router->map( 'PUT', $prefix.'/products/[i:id]', 'ProductController#update', 'product_update');
+$router->map( 'DELETE', $prefix.'/products/[i:id]', 'ProductController#delete', 'product_delete');
+$router->map( 'POST', $prefix.'/products', 'ProductController#create', 'product_create');
 
 # Routes API Category
-$router->map( 'GET', $prefix.'/categories', 'CategoryController#read', 'read_categories');
-$router->map( 'GET', $prefix.'/categories/[i:id]', 'CategoryController#read_one', 'read_one_category');
-$router->map( 'GET', $prefix.'/categories/search', 'CategoryController#search', 'search_category');
-$router->map( 'GET', $prefix.'/categories/first', 'CategoryController#readFirst', 'read_category_first');
-$router->map( 'GET', $prefix.'/categories/last', 'CategoryController#readLast', 'read_category_last');
-$router->map( 'GET', $prefix.'/categories/count', 'CategoryController#count', 'read_category_count');
-$router->map( 'GET', $prefix.'/categories/[i:id]/products', 'CategoryController#read_categories_as_products', 'read_one_category_as_products');
+$router->map( 'GET', $prefix.'/categories', 'CategoryController#index', 'categoy_index');
+$router->map( 'GET', $prefix.'/categories/[i:id]', 'CategoryController#show', 'category_show');
+$router->map( 'GET', $prefix.'/categories/first', 'CategoryController#first', 'category_first');
+$router->map( 'GET', $prefix.'/categories/last', 'CategoryController#last', 'category_last');
+$router->map( 'GET', $prefix.'/categories/count', 'CategoryController#count', 'category_count');
+$router->map( 'GET', $prefix.'/categories/search', 'CategoryController#search', 'category_search');
+$router->map( 'GET', $prefix.'/categories/[i:id]/products', 'CategoryController#category_product', 'category_as_products');
 
-$router->map( 'PUT', $prefix.'/categories/[i:id]', 'CategoryController#update', 'update_category');
-$router->map( 'DELETE', $prefix.'/categories/[i:id]', 'CategoryController#delete', 'delete_category');
-$router->map( 'POST', $prefix.'/categories', 'CategoryController#create', 'create_category');
+$router->map( 'PUT', $prefix.'/categories/[i:id]', 'CategoryController#update', 'category_update');
+$router->map( 'DELETE', $prefix.'/categories/[i:id]', 'CategoryController#delete', 'category_delete');
+$router->map( 'POST', $prefix.'/categories', 'CategoryController#create', 'category_create');
