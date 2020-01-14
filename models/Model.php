87fb0ac56fab9fa90bfcbd84c6883f738cfba9a7
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-14 02:53:06 +0100

    implements base methods

--- models/Model.php
+++ models/Model.php
@@ -1,60 +1,95 @@
 <?php
 
-class Model
-{
+class Model{
 
-    protected $table;
+    protected static $table = null;
 
-    function init() {
-      $this->table = static::tableName();
+    public static function init() {
+      self::$table = self::tableName();
     }
     
-    public static function find($id)
+    public static function getName(){
+      return self::$table;
+    }
+
+    public static function find($id){
+      $tableName = self::$table;
+      return R::getRow("SELECT * FROM $tableName WHERE id = ?", [$id]);
+    }
+
+    public static function findBy($attribute, $value){
+      $tableName = self::$table;
+      return R::getRow("SELECT * FROM $tableName WHERE $attribute = ? LIMIT 1", [$value]);
+    }
+
+    public static function findAll(){
+      $tableName = self::$table;
+      return R::getAll("SELECT * FROM $tableName");
+    }
+
+    public static function findAllBy($attribute, $value){
+      $tableName = self::$table;
+      return R::getAll("SELECT * FROM $tableNameable WHERE $attribute = ?", [$value]);
+    }
+
+    public static function first(){
+      $tableName = self::$table;
+      return R::getRow("SELECT * FROM $tableName ORDER BY id ASC LIMIT 0,1");
+    }
+    
+    public static function last(){
+      $tableName = self::$table;
+      return R::getRow("SELECT * FROM $tableName ORDER BY id DESC LIMIT 0,1");
+    }
+    
+    public static function count(){
+      $tableName = self::$table;
+      return R::getRow("SELECT COUNT(*) as total_rows FROM $tableName");
+    }
+
+    public static function searchBy($attributes_values){
+      $query = "";
+      $index = 0;
+      $keys = array_keys($attributes_values);
+      $values;
+
+      foreach($keys as $key){
+        $query = $query . key . " LIKE ?";
+        if ($index != count($attributes_values)-1){
+          $query = $query . " AND ";
+        }
+        array_push($values, "%".$attributes_values[key]."%");
+      }
+
+      $tableName = self::$table;
+      return R::getAll("SELECT * FROM $tableName WHERE $query ORDER BY name ASC", $values );
+    }
+  
+
+    public function delete()
     {
-     
+      
+    }
+    
+    protected function update()
+    {
+    
     }
 
-  public static function findBy($attribute, $value)
-  {
-   
-  }
+    public function save()
+    {
+        $table_name = self::$table;
+        $attributes = get_object_vars($this);
+        $attribute_names = array_keys($attributes);
+        $query = "INSERT INTO $table_name (".join(',', $attribute_names).") VALUES (".join(',', array_map(function ($item) { return '?';}, $attribute_names)).")";
+        R::exec($query, $attribute_names);
+        return 1;
+    }
 
-  public static function findAll()
-  {
-   
-  }
-
-  public static function findAllBy($attribute, $value)
-  {
-   
-  }
-  
-  
-  public function save()
-  {
-    
-  }
-  
-  public function destroy()
-  {
-    
-  }
-  
-  protected function update()
-  {
-   
-  }
-
-  protected function insert()
-  {
-   
-  }
-
-  protected static function tableName()
-  {
-    $class_name = strtolower(get_called_class());
-    preg_match('/\\\\(.+?)$/', $class_name, $matches);
-    return preg_match("/{'_'}/i", $class_name) ? $matches[1] : $matches[1]."s";
-  }
+    protected static function tableName()
+    {
+      $class_name = strtolower(get_called_class());
+      return preg_match("/{'_'}/i", $class_name) ? $class_name : $class_name."s";
+    }
 
 }
