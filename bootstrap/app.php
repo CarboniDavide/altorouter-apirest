cd7bf0856550caac8525911c8bf785a98d8a7c51
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-15 17:45:42 +0100

    config db an load orm in app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,21 @@
 <?php
 
-Load::All();
-Database::setup();
+// load app configuration
+require_once BASE_DIR .'/config.php';
 
-require_once (BASE_DIR . "/routes/dispatcher.php");
+// include DB ORM
+include_once BASE_DIR . '/includes/rb-mysql.php';
+
+// database load config and set connection
+$host = M_DB_HOST;
+$db_name = M_DB_NAME;
+$username = M_DB_USER;
+$password = M_DB_PASS;
+
+R::setup("mysql:host=$host;dbname=$db_name", $username, $password);
+
+// include routes
+require_once BASE_DIR . "/routes/dispatcher.php";
+
+
+
