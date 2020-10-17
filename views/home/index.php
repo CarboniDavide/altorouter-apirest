e5a89044ead18e9dfcc6d20e977d728eda11beae
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-10-17 05:33:24 +0200

    add api link

--- views/home/index.php
+++ views/home/index.php
@@ -1,6 +1,7 @@
 
 <div class = "d-flex align-items-center justify-content-center flex-column" id="home">
     <img src="img/catalogue.png" height="339" width="342"/>
-    <a href="products"><buttom class = "btn btn-primary">Products</buttom></a>
+    <a href="/products"><buttom class = "btn btn-primary">Products</buttom></a>
     <a href="/categories"><buttom class = "btn btn-danger">Categories</buttom></a>
+    <a href="/api/v1"><buttom class = "btn btn-success">API</buttom></a>
 </div>
